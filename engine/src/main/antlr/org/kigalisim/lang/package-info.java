3c1e9cdc5d8b7693e679777177796c3201e743e0
A Samuel Pottinger <sam@gleap.org>
2025-10-30 15:24:13 -0700

    Add package-info corresponding to subdir readmes. (#631)

---
+++ engine/src/main/antlr/org/kigalisim/lang/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * <strong>ANTLR grammar</strong>
+ *
+ * <p>This package is only expected to contain the grammar for QubecTalk with all other classes
+ * generated through build. For more information, see the <a
+ * href="https://kigalisim.org/guide/qubectalk.pdf">QubecTalk specification</a>.
+ *
+ * @license BSD-3-Clause
+ */
+package org.kigalisim.lang;
