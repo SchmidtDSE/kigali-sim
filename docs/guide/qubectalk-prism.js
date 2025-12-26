5e9324f6f6d404dbc565e7180aae91a9d1729dc4
Sam Pottinger <sam.pottinger@berkeley.edu>
2025-12-26 19:14:48 +0000

    Add % prior year and % current grammar support

--- docs/guide/qubectalk-prism.js
+++ docs/guide/qubectalk-prism.js
@@ -19,9 +19,9 @@ Prism.languages.qubectalk = {
     pattern: new RegExp("\\b(?:" + [
       "start", "end", "default", "policy", "simulations", "application",
       "substance", "define", "uses", "modify", "simulate", "about",
-      "variables", "across", "as", "at", "by", "cap", "change", "charge",
+      "variables", "across", "as", "at", "by", "cap", "change", "charge", "current",
       "during", "enable", "eol", "equals", "floor", "for", "from", "get", "in", "induction", "initial",
-      "of", "recharge", "recover", "replace", "replacement", "retire", "reuse", "set",
+      "of", "prior", "recharge", "recover", "replace", "replacement", "retire", "reuse", "set",
       "then", "to", "trials", "using", "with", "displacing", "and", "or",
       "xor", "if", "else", "endif", "normally", "sample", "std",
       "uniformly", "limit", "annually", "beginning", "day", "days",
